3ce7555f05aeaf4d7346acec4b6ee842a3860fbc
lzlxxx <2305249128@qq.com>
2025-03-18 17:16:26 +0800

    完成接口封装

--- 14-go-IM-System/server.go
+++ 14-go-IM-System/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -47,13 +48,30 @@ func (this *Server) BroadCast(user *User, msg string) {
 func (this *Server) Handler(conn net.Conn) {
 	//...当前连接的业务
 	//fmt.Println("连接建立成功")
-	user := NewUser(conn)
-	//用户上线。将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
-	//广播当前用户上线
-	this.BroadCast(user, "已上线")
+	user := NewUser(conn, this)
+	user.Online()
+
+	//接受客户端发送的数据
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn read err:", err)
+				return
+			}
+			//提取用户的消息(去除‘\n’)
+			msg := string(buf[:n-1])
+			//将得到的信息进行广播
+			user.DoMessage(msg)
+		}
+
+	}()
+
 	//当前handler阻塞
 	select {}
 }
--- 14-go-IM-System/user.go
+++ 14-go-IM-System/user.go
@@ -3,23 +3,50 @@ package main
 import "net"
 
 type User struct {
-	Name string
-	Addr string      //当前客户端地址
-	C    chan string //每个用户的channel
-	conn net.Conn
+	Name   string
+	Addr   string      //当前客户端地址
+	C      chan string //每个用户的channel
+	conn   net.Conn
+	server *Server
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String() //当前客户端地址
 	user := &User{
-		Name: userAddr,
-		Addr: userAddr,
-		C:    make(chan string),
-		conn: conn,
+		Name:   userAddr,
+		Addr:   userAddr,
+		C:      make(chan string),
+		conn:   conn,
+		server: server,
 	}
 	//启动监听当前user，channel消息的goroutine
 	go user.ListenMessage()
 	return user
+}
+
+// 用户上线业务
+func (this *User) Online() {
+	//用户上线。将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+	//广播当前用户上线
+	this.server.BroadCast(this, "已上线")
+}
+
+// 用户下线业务
+func (this *User) Offline() {
+	//用户下线。将用户从onlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+	//广播当前用户上线
+	this.server.BroadCast(this, "下线")
+
+}
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
+
 }
 
 // 监听当前User channel的方法，一旦有消息就直接发送给对端客户端
