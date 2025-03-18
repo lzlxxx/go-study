739f4a2998dfcd26b6008e40b55f019d3be2118a
lzlxxx <2305249128@qq.com>
2025-03-18 20:35:51 +0800

    客户端实现，更新用户名

--- 14-go-IM-System/client/client.go
+++ 14-go-IM-System/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -30,6 +32,13 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// 处理server回应的信息，直接显示到标准输出即可
+func (client *Client) DealResponse() {
+	//一旦client.conn有数据，直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+
+}
+
 func (client *Client) menu() bool {
 	var flag int
 	fmt.Println("1.公聊模式")
@@ -45,6 +54,17 @@ func (client *Client) menu() bool {
 		return false
 	}
 
+}
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>请输入用户名:")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println(">>>>>>>>>发送消息失败：", err)
+		return false
+	}
+	return true
 }
 func (client *Client) Run() {
 	for client.flag != 0 {
@@ -62,7 +82,7 @@ func (client *Client) Run() {
 			break
 		case 3:
 			//更新用户名
-			fmt.Println("更新用户名模式选择...")
+			client.UpdateName()
 			break
 		}
 	}
@@ -85,6 +105,8 @@ func main() {
 		fmt.Println(">>>>>>>>>链接服务器失败")
 		return
 	}
+	//单独开启一个goroutine来处理server的回执消息
+	go client.DealResponse()
 	fmt.Println(">>>>>>>>>链接服务器成功")
 	client.Run()
 }
